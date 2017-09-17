0e3025f535ee7b0d750aef2c9e8f32f318b3303e
dell <dell@DESKTOP-ML61NII>
2017-09-17 16:55:44 +0800

    123

--- Server/Server/Conductor.cpp
+++ Server/Server/Conductor.cpp
@@ -1,5 +1,9 @@
 #include "Conductor.h"
+#include <iostream>
+#include "Ticket.h"
 #include <cstring>
+using namespace std;
+
 
 Conducor::Conducor(char *i, char *p) :User(i, p), innerPassword("12345") {}
 
@@ -7,12 +11,15 @@ bool Conducor::inquireSuser(char *p, char *i, char **out)
 {
 	if (this->check(p))
 	{
-		mysql_real_query(&con, strcat("SELECT * FROM suser where id=", i), 36);
+		char a[56];
+		sprintf(a, "SELECT password,name FROM suser where id='%s'", i);
+		mysql_real_query(&con, a, 56);
 		if (res = mysql_store_result(&con))
 		{
 			result = mysql_fetch_row(res);
-			strcpy(out[? ], i);
-			strcpy(out[? ], result[0]);
+			strcpy(out[0], i);
+			strcpy(out[1], result[0]);
+			strcpy(out[2], result[1]);
 		}
 		else
 			return false;
@@ -29,12 +36,12 @@ bool Conducor::check(char *p)
 		return false;
 }
 
-bool Conducor::addSuser(char *p, char *i, char *pa)
+bool Conducor::addSuser(char *p, char *i, char *pa, char* na)
 {
 	if (check(p))
 	{
 		char tem[100];
-		sprintf(tem, "INSERT INTO suser(id,password) VALUES('%s','%s')", i, pa);
+		sprintf(tem, "INSERT INTO suser(id,password,name) VALUES('%s','%s','%s')", i, pa, na);
 		if (mysql_query(&con, tem))
 			return true;
 		else
@@ -49,7 +56,7 @@ bool Conducor::changeSuserInfo(char *p, char *mid, char *aid, char *pa)
 	if (check(p))
 	{
 		char tem[200];
-		sprintf(tem, "UPDATE TABLE suser set id=%s ,password=%s where id=%s", aid, pa, mid);
+		sprintf(tem, "UPDATE suser set id='%s' ,password='%s' where id='%s'", aid, pa, mid);
 		if (mysql_query(&con, tem))
 			return true;
 		else
@@ -64,7 +71,7 @@ bool Conducor::deleteUser(char *p, char *i)
 	if (check(p))
 	{
 		char tem[200];
-		sprintf(tem, "DELETE FROM suser where id=%s", i);
+		sprintf(tem, "DELETE FROM suser where id='%s'", i);
 		if (mysql_query(&con, tem))
 			return true;
 		else
@@ -74,24 +81,127 @@ bool Conducor::deleteUser(char *p, char *i)
 		return false;
 }
 
-bool Conducor::inquireCoach(char *p, char *, char ***)
+
+bool Conducor::addCoach(char *p, char *i, char *st, char ***station, int s, char *seat)
 {
-	return false;
+	if (check(p))
+	{
+		char temp[100];
+		sprint("select id from coach where id='%s'", i);
+		mysql_real_query(&con, temp, 40);
+		if (mysql_store_result(&con))
+			return false;
+		else
+		{
+			sprintf(temp, "insert into coach(id,starttime,seatnum) values('%s','%s',%s)", i, st, seat);
+			if (mysql_query(&con, temp))
+			{
+				sprintf(temp, "insert into ctos(id) values('%s')", i);
+				if (mysql_query(&con, temp))
+				{
+					for (int a = 0; a < s; a++)
+					{
+						sprintf(temp, "select num from stov where station='%s'", station[a][0]);
+						mysql_real_query(&con, temp, 100);
+						if (res = mysql_store_result(&con))
+						{
+							result = mysql_fetch_row(res);
+							sprintf(temp, "insert into ctov(num,id,station) values(%d,'%s','%s')", a, i, station[a][0]);
+							if (mysql_query(&con, temp))
+							{
+								sprintf(temp, "update ctos set %s='%s,%s' where id='%s'", result[0], station[a][1], station[a][2], i);
+								if (mysql_query(&con, temp))
+								{
+									continue;
+								}
+								else
+								{
+									while (!mysql_query(&con, strcat(strcat("delete from coach where id='", i), "'")));
+									while (!mysql_query(&con, strcat(strcat("delete from ctov where id='", i), "'")));
+									if (!a)
+										while (!mysql_query(&con, strcat(strcat("delete from ctos where id='", i), "'")));
+									return false;
+								}
+							}
+							else
+							{
+								while (!mysql_query(&con, strcat(strcat("delete from coach where id='", i), "'")));
+								if (!a)
+									while (!mysql_query(&con, strcat(strcat("delete from ctov where id='", i), "'")));
+								return false;
+							}
+						}
+						else
+						{
+							int max = 0;
+							mysql_real_query(&con, "select max(num) from stov", 27);
+							if (res = mysql_store_result(&con))
+							{
+								max = str2int(mysql_fetch_row(res)[0]);
+							}
+							++max;
+							sprintf(temp, "alert table ctos add(%d varchar(9) default '-1' not null)", max);
+							while (!mysql_query(&con, temp));
+							sprintf(temp, "update ctos set %d='-1'", max);
+							while (!mysql_query(&con, temp));
+							sprintf(temp, "insert into ctov(num,id,station) values(%d,'%s','%s')", a, i, station[a][0]);
+							if (mysql_query(&con, temp))
+							{
+								sprintf(temp, "update ctos set %d='%s,%s' where id='%s'", max, station[a][1], station[a][2], i);
+								if (mysql_query(&con, temp))
+								{
+									continue;
+								}
+								else
+								{
+									while (!mysql_query(&con, strcat(strcat("delete from coach where id='", i), "'")));
+									while (!mysql_query(&con, strcat("delete from ctov where id=", i)));
+									if (!a)
+										while (!mysql_query(&con, strcat(strcat("delete from ctos where id='", i), "'")));
+									return false;
+								}
+							}
+							else
+							{
+								while (!mysql_query(&con, strcat(strcat("delete from coach where id='", i), "'")));
+								if (!a)
+									while (!mysql_query(&con, strcat(strcat("delete from ctov where id=", i), "'")));
+								return false;
+							}
+						}
+					}
+					return true;
+				}
+				else
+				{
+					while (!mysql_query(&con, strcat(strcat("delete from coach where id='", i), "'")));
+					return false;
+				}
+			}
+			else
+				return false;
+		}
+	}
+	else
+		return false;
 }
 
-bool Conducor::addCoach(char *p, char *, char *, char *, char *)
+bool Conducor::deleteCoach(char *p, char *i)
 {
-	return false;
-}
-
-bool Conducor::deleteCoach(char *p, char *)
-{
-	return false;
+	if (check(p))
+	{
+		while (!mysql_query(&con, strcat(strcat("delete from coach where id='", i), "'")));
+		while (!mysql_query(&con, strcat(strcat("delete from ctov where id=", i), "'")));
+		while (!mysql_query(&con, strcat(strcat("delete from ctos where id='", i), "'")));
+		return true;
+	}
+	else
+		return false;
 }
 
 char Conducor::verify(char *p)
 {
-	if (this->id == "???" && p == "???")
+	if (this->id == "12306" && p == "54321")
 	{
 		return 'Y';
 	}
