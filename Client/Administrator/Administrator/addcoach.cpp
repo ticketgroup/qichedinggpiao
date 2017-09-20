e29bcb0857baa6ff56270da3af14293c256cf217
MizunashiAkari <mzns_utopia@outlook.com>
2017-09-20 11:23:49 +0800

    client 0.0.1

--- Client/Administrator/Administrator/addcoach.cpp
+++ Client/Administrator/Administrator/addcoach.cpp
@@ -16,11 +16,36 @@ Addcoach::~Addcoach()
 
 void Addcoach::on_pushButton_clicked()
 {
-    switch(QMessageBox::question(NULL, "question", "Content", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes))
+
+    switch(QMessageBox::question(NULL, QString::fromLocal8Bit("确认增加"), QString::fromLocal8Bit("确认增加这条车辆信息吗？"), QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes))
     {
     case QMessageBox::Yes:
-        QMessageBox::about(NULL, "About", "About this application");
-        this->close();
+        char st[2];
+        QString str = "";
+        QTableWidget *table = ui->tableWidget_2;
+        int i = 0;
+        for(; table->item(i,0)!=0; i++)
+        {
+            str += (table->item(i,0)->text() + ";");
+        }
+        if(i < 8 || (i - 1)%3 != 0 )
+        {
+            QMessageBox::about(NULL, QString::fromLocal8Bit("增加失败"),  QString::fromLocal8Bit("请填充好汽车的基本信息！"));
+            break;
+        }
+        else
+        {
+            client.sendmsg("28", str, st, 335, 2);
+            if(st[0] == 'Y')
+            {
+                QMessageBox::about(NULL, QString::fromLocal8Bit("增加成功"), QString::fromLocal8Bit("增加成功！"));
+                this->close();
+            }
+            else if(st[0] == 'N')
+            {
+                QMessageBox::about(NULL, QString::fromLocal8Bit("增加失败"), QString::fromLocal8Bit("增加失败！请重新尝试。"));
+            }
+        }
         break;
     case QMessageBox::No:
         break;
