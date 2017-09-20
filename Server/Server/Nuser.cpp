906bed2474647a7442be70d312efc1794a7ca9dc
MizunashiAkari <mzns_utopia@outlook.com>
2017-09-20 10:47:12 +0800

    server v0.0.1高清重制版

--- Server/Server/Nuser.cpp
+++ Server/Server/Nuser.cpp
@@ -16,40 +16,40 @@ bool Nuser::inquireticket(char*** info)
 	char t[5];
 	sprintf(temp, "select id,ticketnum,date,passenger,seatid from ticket where buyer='%s';", id);
 	mysql_real_query(&coa, temp, 80);
-	cres = mysql_store_result(&coa);
-	int i = 0;
-	for (; row = mysql_fetch_row(cres); i++)
+	if (cres = mysql_store_result(&coa))
 	{
-		strcpy(info[i][0], row[0]);
-		strcpy(info[i][1], row[4]);
-		strcpy(info[i][7], row[2]);
+		int i = 0;
+		for (; row = mysql_fetch_row(cres); i++)
+		{
+			strcpy(info[i][0], row[0]);
+			strcpy(info[i][1], row[4]);
+			strcpy(info[i][7], row[2]);
 
-		sprintf(temp, "select name from cinfo where id='%s';", row[3]);
-		mysql_real_query(&con, temp, 50);
-		res = mysql_store_result(&con);
-		strcpy(info[i][6], mysql_fetch_row(res)[0]);
+			sprintf(temp, "select name from cinfo where id='%s';", row[3]);
+			mysql_real_query(&con, temp, 50);
+			res = mysql_store_result(&con);
+			strcpy(info[i][6], mysql_fetch_row(res)[0]);
 
-		getse(row[0], row[1], info[i][2], info[i][4]);
-		sprintf(temp, "select starttime from coach where id='%s';", info[i][0]);
-		mysql_real_query(&coa, temp, 60);
-		cres = mysql_store_result(&coa);
-		strcpy(st, mysql_fetch_row(cres)[0]);
-		sprintf(temp, "select (select num from stov where station=%s),(select num from stov where station=%s) from ctos where id='%s';", info[i][2], info[i][4], info[i][0]);
-		mysql_real_query(&con, temp, 150);
-		cres = mysql_store_result(&con);
-		row = mysql_fetch_row(cres);
+			getse(row[0], row[1], info[i][2], info[i][4]);
+			sprintf(temp, "select starttime from coach where id='%s';", info[i][0]);
+			mysql_real_query(&coa, temp, 60);
+			cres = mysql_store_result(&coa);
+			strcpy(st, mysql_fetch_row(cres)[0]);
+			sprintf(temp, "select (select num from stov where station=%s),(select num from stov where station=%s) from ctos where id='%s';", info[i][2], info[i][4], info[i][0]);
+			mysql_real_query(&con, temp, 150);
+			cres = mysql_store_result(&con);
+			row = mysql_fetch_row(cres);
 
-		sprintf(t, "%04s", strtok(row[0], ","));
-		timeAdd(st, t, info[i][3]);
-		sprintf(t, "%04s", strtok(row[1], ","));
-		timeAdd(st, t, info[i][5]);
+			sprintf(t, "%04s", strtok(row[0], ","));
+			timeAdd(st, t, info[i][3]);
+			sprintf(t, "%04s", strtok(row[1], ","));
+			timeAdd(st, t, info[i][5]);
 
+		}
+		strcat(info[i - 1][7], "$");
 	}
-	strcat(info[i - 1][7], "$");
-	/*************
-	template
-	*************/
-	return true;
+	else
+		return true;	
 }
 bool ver(char *Id, char *num)
 {
