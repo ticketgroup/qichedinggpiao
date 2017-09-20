232b603b08e8e2224e5790bc38181c00779e8ce9
MizunashiAkari <mzns_utopia@outlook.com>
2017-09-20 16:04:04 +0800

    Client Alpha Version

--- Client/Administrator/Administrator/changecoach.cpp
+++ Client/Administrator/Administrator/changecoach.cpp
@@ -9,7 +9,6 @@ Changecoach::Changecoach(QWidget *parent) :
     QTableWidget *table = ui->tableWidget_2;
     table->setSelectionMode(QAbstractItemView::SingleSelection);
     setFixedSize(this->width(), this->height());
-
 }
 
 Changecoach::~Changecoach()
@@ -25,11 +24,35 @@ Passwordchange::setId(QString i)
 
 void Changecoach::on_pushButton_clicked()
 {
-    switch(QMessageBox::question(NULL, "question", "Content", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes))
+    switch(QMessageBox::question(NULL, QString::fromLocal8Bit("确认修改"), QString::fromLocal8Bit("确认修改这条车辆信息吗？"), QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes))
     {
     case QMessageBox::Yes:
-        QMessageBox::about(NULL, "About", "About this application");
-        this->close();
+        char st[2];
+        QString str = id + ";";
+        QTableWidget *table = ui->tableWidget_2;
+        int i = 0;
+        for(; table->item(i,0)!=0; i++)
+        {
+            str += (table->item(i,0)->text() + ";");
+        }
+        if(i < 8 || (i - 1)%3 != 0 )
+        {
+            QMessageBox::about(NULL, QString::fromLocal8Bit("修改失败"),  QString::fromLocal8Bit("请填充好汽车的基本信息！"));
+            break;
+        }
+        else
+        {
+            client.sendmsg("28", str, st, 346, 2);
+            if(st[0] == 'Y')
+            {
+                QMessageBox::about(NULL, QString::fromLocal8Bit("修改成功"), QString::fromLocal8Bit("修改成功！"));
+                this->close();
+            }
+            else if(st[0] == 'N')
+            {
+                QMessageBox::about(NULL, QString::fromLocal8Bit("修改失败"), QString::fromLocal8Bit("修改失败！请重新尝试。"));
+            }
+        }
         break;
     case QMessageBox::No:
         break;
