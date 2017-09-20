49eb6f3f4d780580e3449985c1dad7499e04bb66
MizunashiAkari <mzns_utopia@outlook.com>
2017-09-20 15:32:46 +0800

    client nuser v0.0.2

--- Client/Dingpiao/dingpiaoxitong/denglu.cpp
+++ Client/Dingpiao/dingpiaoxitong/denglu.cpp
@@ -19,7 +19,7 @@ denglu::~denglu()
 
 void denglu::on_pushButton_3_clicked()
 {
-    /*QString id = ui->nLineEdit_2->text();
+    QString id = ui->nLineEdit_2->text();
     QString password = ui->pLineEdit_2->text();
     QRegExp rx("^\\d{11}$");
     QRegExp rp("\\W");
@@ -34,31 +34,31 @@ void denglu::on_pushButton_3_clicked()
             {
                 if(st[0] == 'Y')
                 {
-                    */
+
                     accept();
-                /*}
+                }
                 else if(st[0] == '1')
                 {
-                    QMessageBox::about(NULL, "登录失败", "没有这个用户！");
+                    QMessageBox::about(NULL, QString::fromLocal8Bit("登录失败"), QString::fromLocal8Bit("没有这个用户！"));
                 }
                 else if(st[0]  == '2')
                 {
-                    QMessageBox::about(NULL, "登录失败", "密码错误！");
+                    QMessageBox::about(NULL, QString::fromLocal8Bit("登录失败"), QString::fromLocal8Bit("密码错误！"));
                 }
             }
             else
-                QMessageBox::about(NULL, "登录失败", "服务器认证失败，请重新登录！");
+                QMessageBox::about(NULL, QString::fromLocal8Bit("登录失败"), QString::fromLocal8Bit("服务器认证失败，请重新登录！"));
 
         }
         else
         {
-            QMessageBox::about(NULL, "登录失败", "密码中含有非法字符！");
+            QMessageBox::about(NULL, QString::fromLocal8Bit("登录失败"), QString::fromLocal8Bit("密码中含有非法字符！"));
         }
     }
     else
     {
-        QMessageBox::about(NULL, "登录失败", "您输入的用户名有误！");
-    }*/
+        QMessageBox::about(NULL, QString::fromLocal8Bit("登录失败"), QString::fromLocal8Bit("您输入的用户名有误！"));
+    }
 }
 
 
